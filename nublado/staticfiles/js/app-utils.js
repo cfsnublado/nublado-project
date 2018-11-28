97ed954df236578320dc24cfd65b1ab8415bf59e
Christopher <ChristopherSanders78@gmail.com>
2018-11-27 23:56:03 -0800

    ...

--- nublado/staticfiles/js/app-utils.js
+++
@@ -1,7 +0,0 @@
-
-function redirectUrl(url, params={}) {
-    for (const key in params) {
-        url = url.replace(key, params[key]);
-    }
-    window.location.replace(url);
-}
--- nublado/staticfiles/js/vue/proto/app.js
+++
@@ -1,9 +0,0 @@
-
-// from testing
-Vue.component('oxford-api', OxfordApi)
-
-// Instantiate main app instance.
-const vm = new Vue({
-  el: '#app-container',
-  delimiters: ['[[', ']]'],
-})

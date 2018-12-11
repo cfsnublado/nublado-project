49481f851ac20668e38d68a31b4d771802a19cb9
Christopher <ChristopherSanders78@gmail.com>
2018-12-11 15:53:37 -0800

    ...

--- nublado/app/tests/selenium/test_vocab.py
+++ nublado/app/tests/selenium/test_vocab.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 
 from django.contrib.auth import get_user_model
@@ -98,30 +99,48 @@ class VocabContextAuthTest(TestCommon):
         )
         self.vocab_context = VocabContext.objects.create(
             vocab_source=self.vocab_source,
-            content='Algo de contenido'
+            content='He likes to eat pizza on Sunday. She likes to eat pizza on Friday.'
         )
         self.vocab_entry = VocabEntry.objects.create(
-            language='es',
-            entry='contenido'
+            language='en',
+            entry='pizza'
         )
 
     def get_tag_xpath(self, tagbox_id=None, tag=None, close=False):
         if tagbox_id and tag:
             if not close:
-                xpath = "//a[contains(., '{0}') and ancestor::div[@id='{1}']]".format(
+                xpath = '//a[contains(., "{0}") and ancestor::div[@id="{1}"]]'.format(
                     tag,
                     tagbox_id
                 )
             else:
                 # Get tag's close button xpath.
-                xpath = "//a[@class='delete-tag' and preceding-sibling::a[contains(., '{0}')] and ancestor::div[@id='{1}']]".format(
+                xpath = '//a[@class="delete-tag" and preceding-sibling::a[contains(., "{0}")] and ancestor::div[@id="{1}"]]'.format(
                     tag,
                     tagbox_id
                 )
             return xpath
 
+    def add_tag(self, tagbox_id=None, tag=None, return_key=False):
+        if tagbox_id and tag:
+            css_selector = '#{0} .autocomplete-input'.format(tagbox_id)
+            tagbox_input = self.get_element_by_css(css_selector)
+            tagbox_input.send_keys(tag)
+        if return_key:
+            tagbox_input.send_keys(u'\ue007')
+
+    def get_highlight_xpath(self, tag):
+        xpath = "//mark[@class='tagged-text' and contains(., '{0}')]".format(tag)
+        return xpath
+
+    def tag_hover(self, tag_element):
+        hover = ActionChains(self.browser).move_to_element(tag_element)
+        hover.perform()
+
     def test_tag_context(self):
-        vocab_entry_tagbox_id = "vocab-entry-tags"
+        vocab_entry_tagbox_id = 'vocab-entry-tags'
+        vocab_entry_instance_tagbox_id = 'vocab-entry-instance-tagbox'
+        vocab_entry_instance_container_id = 'vocab-entry-instance-tags'
 
         self.browser.get('{0}{1}'.format(
             self.live_server_url,
@@ -132,22 +151,55 @@ class VocabContextAuthTest(TestCommon):
         ))
         self.login_user(self.user.username)
         self.page_load(page_titles['page_vocab_context_tag_title_en'])
+
+        # Context entry doesn't exist yet.
         self.assertFalse(
             VocabContextEntry.objects.filter(
                 vocab_context_id=self.vocab_context.id,
                 vocab_entry_id=self.vocab_entry.id
             ).exists()
         )
+
+        # Search and select entry through autocompilete.
         link = self.search_autocomplete_by_language(
             self.vocab_entry.language,
             self.vocab_entry.entry
         )
         link.click()
+
+        # Load tag in vocab entry tagbox and select it. Vocab entry instance tagbox appears.
         vocab_entry_xp = self.get_tag_xpath(tagbox_id=vocab_entry_tagbox_id, tag=self.vocab_entry.entry)
         self.wait.until(EC.element_to_be_clickable((By.XPATH, vocab_entry_xp)))
+        self.get_element_by_xpath(vocab_entry_xp).click()
+        self.wait.until(EC.element_to_be_clickable((By.ID, vocab_entry_instance_tagbox_id)))
+
+        # Context entry now exists.
         self.assertTrue(
             VocabContextEntry.objects.filter(
                 vocab_context_id=self.vocab_context.id,
                 vocab_entry_id=self.vocab_entry.id
             ).exists()
         )
+        vocab_context_entry = VocabContextEntry.objects.get(
+            vocab_context_id=self.vocab_context,
+            vocab_entry_id=self.vocab_entry
+        )
+        self.assertEqual(list(vocab_context_entry.get_vocab_entry_tags()), [])
+
+        # Add tag to vocab context entry.
+        self.add_tag(
+            tagbox_id=vocab_entry_instance_tagbox_id,
+            tag=self.vocab_entry.entry,
+            return_key=True
+        )
+        vocab_entry_instance_xp = self.get_tag_xpath(
+            tagbox_id=vocab_entry_instance_container_id,
+            tag=self.vocab_entry.entry
+        )
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, vocab_entry_instance_xp)))
+        vocab_entry_instance_tag = self.get_element_by_xpath(vocab_entry_instance_xp)
+
+        # Vocab entry instance is highlighted in text.
+        vocab_entry_highlight_xp = self.get_highlight_xpath(tag=self.vocab_entry.entry)
+        vocab_entry_highlighted = self.get_elements_by_xpath(vocab_entry_highlight_xp)
+        self.assertEqual(len(vocab_entry_highlighted), 2)

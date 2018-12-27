202519d6ae92c6d224b57aba9adce6a76ad1c957
Christopher <ChristopherSanders78@gmail.com>
2018-12-27 00:32:43 -0800

    ...

--- nublado/vocab/management/commands/backup_project.py
+++ nublado/vocab/management/commands/backup_project.py
@@ -1,7 +1,8 @@
 import getpass
 import json
-import os
+from pathlib import Path
 
+from django.conf import settings
 from django.contrib.auth import authenticate
 from django.core.management.base import BaseCommand, CommandError
 
@@ -24,17 +25,28 @@ class Command(BaseCommand):
             raise CommandError('Invalid login')
 
     def add_arguments(self, parser):
-        parser.add_argument('output_path', nargs=1, type=str)
+        parser.add_argument('--output_path', nargs=1, type=str)
 
     def handle(self, *args, **options):
         user = self.login_user()
+        if options['output_path']:
+            base_dir = Path(options['output_path'][0])
+        else:
+            base_dir = Path('{0}/docs/vocab_json'.format(settings.BASE_DIR))
+            base_dir.mkdir(parents=True, exist_ok=True)
 
         vocab_projects = VocabProject.objects.filter(owner=user)
+
         for vocab_project in vocab_projects:
+            project_dir = base_dir / vocab_project.slug
+            project_dir.mkdir(parents=True, exist_ok=True)
+
             vocab_sources = vocab_project.vocab_sources.all()
+
             for vocab_source in vocab_sources:
                 vocab_source_dict = export_vocab_source(vocab_source=vocab_source)
-                file_path = os.path.join(options['output_path'][0], vocab_source.slug)
+                filename = project_dir / '{0}.json'.format(vocab_source.slug)
 
-                with open(file_path, 'w+') as f:
+                with filename.open('w+') as f:
                     f.write(json.dumps(vocab_source_dict, indent=2))
+                    self.stdout.write(self.style.SUCCESS(filename))

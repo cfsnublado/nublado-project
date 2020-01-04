ae2d20818b53e77674ac5f4c857fe6f80054553c
Christopher <ChristopherSanders78@gmail.com>
2020-01-04 15:07:42 -0800

    ...

--- nublado/core/urls.py
+++ nublado/core/urls.py
@@ -7,7 +7,7 @@ from core.views import home_files, render_js, UploadView
 app_name = 'core'
 
 urlpatterns = [
-    re_path('(?P<filename>(robots.txt)|(humans.txt)|(google84b343e306e5cb32.html))', home_files, name='home_files'),
+    re_path('(?P<filename>(robots.txt)|(humans.txt)|(googleb7077bd8f7f710c0.html))', home_files, name='home_files'),
     re_path('media/(?P<path>.*)', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path('upload/', UploadView.as_view(), name='upload_ajax'),
     path('js-settings/', render_js, name='js_settings',),

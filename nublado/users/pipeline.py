b4e0801bc69ee443bd95949c1fa1f8dba72232d1
Christopher <ChristopherSanders78@gmail.com>
2020-01-07 14:34:44 -0800

    Added google oauth2 login capability.

--- nublado/users/pipeline.py
+++ nublado/users/pipeline.py
@@ -1,4 +1,11 @@
+from .models import Profile
+
+
 def get_avatar(backend, strategy, details, response, user=None, *args, **kwargs):
     if backend.name == "google-oauth2":
-        user.image_url = response["picture"]
+        image_url = response.get("picture", None)
 
+        if image_url is not None:
+            profile = Profile.objects.get(user_id=user.id)
+            profile.avatar_url = image_url
+            profile.save()
--- nublado/users/templatetags/profile_image_tags.py
+++ nublado/users/templatetags/profile_image_tags.py
@@ -17,25 +17,40 @@ IMG_CIRCLE = "circle"
 def profile_image_url(user, size=settings.USERS_IMAGE_DEFAULT_SIZE):
     if settings.USERS_USE_GRAVATAR:
         params = {"s": str(size)}
+
         if settings.USERS_GRAVATAR_DEFAULT:
             params["d"] = settings.USERS_GRAVATAR_DEFAULT
+
         path = "{0}/?{1}".format(
             hashlib.md5(force_bytes(user.email.lower())).hexdigest(), urlencode(params)
         )
+
         return urljoin(settings.USERS_GRAVATAR_BASE_URL, path)
 
 
 @register.simple_tag
-def profile_image(user, size=settings.USERS_IMAGE_DEFAULT_SIZE, border_radius=0, **kwargs):
+def profile_image(
+    user,
+    size=settings.USERS_IMAGE_DEFAULT_SIZE,
+    border_radius=0,
+    **kwargs
+):
     alt = user.username
-    url = profile_image_url(user, size)
+
+    if user.profile.avatar_url:
+        url = user.profile.avatar_url
+    else:
+        url = profile_image_url(user, size)
+
     if border_radius == IMG_CIRCLE:
         border_radius = int(size / 2)
-    context = dict(kwargs, **{
+
+    image_context = dict(kwargs, **{
         "user": user,
         "url": url,
         "alt": alt,
         "size": size,
         "border_radius": border_radius
     })
-    return render_to_string("users/profile_image/profile_image_tag.html", context)
+
+    return render_to_string("users/profile_image/profile_image_tag.html", image_context)
